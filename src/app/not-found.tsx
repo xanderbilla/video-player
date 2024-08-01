9de9c05ce78b901d759c11e95fafbb04e15e2161
Vikas <xanderbilla@gmail.com>
2024-08-01 20:22:00 +0530

    C2# Valid till API is added

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import React from 'react'
 
 type Props = {}
@@ -6,6 +7,11 @@ export default function NotFound({}: Props) {
   return (
     <div className="h-screen">
       <span>Video Source Not Found! | ID: Unknown</span>
+      <div className="">
+        <Link href="/?view=9ce3b1b1-1b1b-11ec-8c4e-0242ac130003">
+        Click Here!
+        </Link>
+      </div>
     </div>
   );
 }
